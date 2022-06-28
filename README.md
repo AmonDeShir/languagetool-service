ffd3b0b3f59a9a7fe0ef5e5d436dbed07f050f8c
Amon De Shir <amonsir29@gmail.com>
2022-06-28 18:06:54 +0200

    Create java service
    /java
    - create java wrapper for the LanguageTool class
    
    /README
    - update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # languagetool-service
+This package is a port of the [schreiben/node-languagetool](https://github.com/schreiben/node-languagetool) to the rust language.
+
+
+This package is a rust binding to the [LanguageTool](https://languagetool.org) spellchecker. It is based on a local instance of LanguageTool. As it is a Java based framework, this binding will use [nullishamy/kate](https://github.com/nullishamy/kate), which provides an embedded local Java Runtime Engine.
+
+##Project stage
+Java part of the project is ready, rust part needs a functionality to ready and run the .jar file. Its functionality will be provided by kate in the [future](https://github.com/nullishamy/kate/issues/13). Therefore, current library development is frozen.
+
+## license
+Project is under LGPL 3 license
---
+++ java/languagetool/src/main/java/pl/amon/Service.java
@@ -0,0 +1,87 @@
+package pl.amon;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObjectBuilder;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.Languages;
+import org.languagetool.rules.Category;
+import org.languagetool.rules.CategoryId;
+import org.languagetool.rules.Rule;
+import org.languagetool.rules.RuleMatch;
+import org.languagetool.rules.patterns.AbstractPatternRule;
+
+
+public class Service {
+  public static void main(String[] args) {
+    System.out.println(Service.check(args[0], args[1]));
+  }
+
+  public static String check(String lang, String data) {
+    try {
+      JLanguageTool tool = new JLanguageTool(Languages.getLanguageForShortCode(lang));
+      return ruleListToString(tool.check(data));
+    }
+    catch (Exception e) {
+      return String.format("{ error: \" %s \" }", e.toString());
+    }
+  }
+
+  private static String ruleListToString(List<RuleMatch> rules) {
+    final Map<String, ?> config = Collections.emptyMap();
+    JsonBuilderFactory factory = Json.createBuilderFactory(config);
+
+    JsonArrayBuilder array = factory.createArrayBuilder();
+    
+    for (RuleMatch match : rules) {
+      System.out.println("Rule!!!");
+
+      Rule rule = match.getRule();
+      Category category = rule.getCategory();
+      CategoryId catId = category.getId();
+      String ruleSubId = "";
+
+      if (rule instanceof AbstractPatternRule) {
+        ruleSubId = ((AbstractPatternRule) rule).getSubId();
+
+        if (ruleSubId == null) {
+          ruleSubId = "";
+        }
+      }
+
+      JsonObjectBuilder matchBuilder = factory.createObjectBuilder()
+        .add("offset", match.getFromPos())
+        .add("length", match.getToPos()-match.getFromPos())
+        .add("message", match.getMessage())
+        .add("shortMessage", match.getShortMessage() == null ? "" : match.getShortMessage())
+        .add("replacements", array(factory, match.getSuggestedReplacements()))
+        .add("ruleId", rule.getId())
+        .add("ruleDescription", rule.getDescription())
+        .add("ruleIssueType", rule.getLocQualityIssueType().toString())
+        .add("ruleSubId", ruleSubId)
+        .add("ruleUrl", rule.getUrl() == null ? "" : rule.getUrl().toString())
+        .add("ruleCategoryId", catId == null ? "" : catId.toString());
+
+      array.add(matchBuilder);
+    }
+
+    JsonObjectBuilder builder = factory.createObjectBuilder().add("match", array);
+    return builder.build().toString();
+  }
+
+  private static JsonArrayBuilder array(JsonBuilderFactory factory, List<String> data) {
+    JsonArrayBuilder array = factory.createArrayBuilder();
+
+    for (String obj : data) {
+      array.add(obj);
+    }
+
+    return array;
+  }
+}
